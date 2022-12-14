eb9e24672182c645022fc42414b8f35f016c5195
Martin Wheeldon <martin.wheeldon@golfnow.co.uk>
2022-12-14 14:09:48 +0000

    Added travis config

--- src/QuoteFactory.php
+++ src/QuoteFactory.php
@@ -10,7 +10,7 @@ class QuoteFactory
         "You made a time machine, out of a Delorean?",
         "Why don't you make like a tree and get outta here?",
         "If my calculations are correct, when this baby hits 88 miles per hour, you're gonna see some serious s***.",
-        "Roads? Where we’re going, we don’t need roads.",
+        "Roads? Where we’re going we don’t need roads.",
         "It’s your kids, Marty! Something’s gotta be done about your kids!"
     ];
 
